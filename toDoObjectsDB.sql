13659dbe3f55db0d66d953524094448d204ab25e
AnaSkvortsova <ana.skvortsova@coveros.com>
2020-08-27 12:00:25 -0400

    ConnectionString12

--- toDoObjectsDB.sql
+++ toDoObjectsDB.sql
@@ -1,6 +1,6 @@
 IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='toDoItems' and xtype='U') 
 CREATE TABLE toDoItems (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY IDENTITY,
     name VARCHAR(255) NOT NULL,
     isComplete BIT NOT NULL
 );
