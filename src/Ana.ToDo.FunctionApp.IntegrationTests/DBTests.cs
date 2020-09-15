369b4a184025a15c40dd5f23661495985454eafa
AnaSkvortsova <ana.skvortsova@coveros.com>
2020-09-15 17:14:10 -0400

    API Tests

--- src/Ana.ToDo.FunctionApp.IntegrationTests/DBTests.cs
+++ src/Ana.ToDo.FunctionApp.IntegrationTests/DBTests.cs
@@ -4,6 +4,7 @@ using Ana.ToDo.FunctionApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 
 namespace Ana.Todo.FunctionApp.IntegrationTests
 {
@@ -47,5 +48,44 @@ namespace Ana.Todo.FunctionApp.IntegrationTests
             Assert.IsTrue(listOfToDoItems.Any());
         }
 
+        [TestMethod]
+        public void deleteToDoItemByIdFromDB_success() 
+        {
+            //arange
+            var newToDo = new ToDoItem {
+                Name = "wash your car",
+                IsComplete = true
+            };
+
+            //act
+            var newItemId = new Repository(connectionDetails).SaveToDoItemToDB(newToDo).Result.Id;
+            var rowsAffected = new Repository(connectionDetails).DeleteItemByIdFromDB(newItemId).Result;
+
+            //assert
+            Assert.AreEqual(1, rowsAffected);
+        }
+
+        [TestMethod]
+        public async Task deleteAllToDoItemsFromDB_success() 
+        {
+            //arange
+            var newToDo1 = new ToDoItem {
+                Name = "wash your car",
+                IsComplete = true
+            };
+
+            var newToDo2 = new ToDoItem {
+                Name = "wash your teeth",
+                IsComplete = true
+            };
+
+            //act
+            await new Repository(connectionDetails).SaveToDoItemToDB(newToDo1);
+            await new Repository(connectionDetails).SaveToDoItemToDB(newToDo2);
+            var rowsAffected = new Repository(connectionDetails).DeleteAllToDoItemsFromDB().Result;
+
+            //assert
+            Assert.IsTrue(rowsAffected >= 2);
+        }
     }
 }
